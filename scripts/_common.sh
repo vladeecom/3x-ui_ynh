d6837bc73c577ca1f6d8f0ac3b6d07549a14b4c4
Vladee <38142274+vladeecom@users.noreply.github.com>
2026-04-11 09:01:36 +0700

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-
-app=$YNH_APP_INSTANCE_NAME
-
-get_paths_and_settings() {
-    install_dir=$(ynh_app_setting_get --app=$app --key=install_dir)
-    data_dir=$(ynh_app_setting_get --app=$app --key=data_dir)
-    domain=$(ynh_app_setting_get --app=$app --key=domain)
-    path_url=$(ynh_app_setting_get --app=$app --key=path)
-}
